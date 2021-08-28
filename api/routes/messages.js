0a9392c86a2a10f9e0d4de60a38a8076412ac581
nshutiremy <remypierrenshuti@gmail.com>
2021-08-28 14:13:46 +0200

    modified private message endpoint with  edit request

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -59,4 +59,48 @@ router.put("/deleteMsg", async (req, res) => {
   }
 });
 
+// edit message
+router.put("/:msgId", async (req, res) => {
+  try {
+    const result = await Message.findByIdAndUpdate(req.params.msgId, {
+      text: req.body.message,
+    });
+    if (result) {
+      res.status(200).json({ msg: "message modified successfully" });
+    } else {
+      res.status(404).json({ msg: "Message Not Found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get messages on specific date
+router.get("/sentOn/:conversationId", async (req, res) => {
+  try {
+    const existingConvo = await Conversation.findById(
+      req.params.conversationId
+    );
+    if (existingConvo) {
+      const today = moment().startOf("day");
+      const messages = await Message.find({
+        conversationId: req.params.conversationId,
+        createdAt: {
+          $gte: today.toDate(),
+          $lte: moment(today).endOf("day").toDate(),
+        },
+      });
+      if (messages) {
+        res.status(200).json({ messages });
+      } else {
+        res.status(404).json({ msg: "Message Not Found" });
+      }
+    } else {
+      res.status(404).json({ msg: "Conversation not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
